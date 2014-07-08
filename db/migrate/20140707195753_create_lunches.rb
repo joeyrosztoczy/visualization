05a4d7c3c31b5111ea74ab1a86a59f9c869e5393
Joey Rosztoczy <joeyr@stotzeq.com>
2014-07-07 20:30:42 -0500

    parsing data

---
+++ db/migrate/20140707195753_create_lunches.rb
@@ -0,0 +1,15 @@
+class CreateLunches < ActiveRecord::Migration
+  def change
+  	create_table :lunches do |t|
+  		t.integer :sponsor_id
+  		t.integer :sponsor_ctds
+  		t.string :sponsor_name
+  		t.integer :site_id
+  		t.integer :site_ctds
+  		t.string :site_name
+  		t.float :percentage_eligible
+  		t.integer :enrollment
+  		t.timestamps
+  	end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
 require 'faker'
+require 'csv'
 
 # create a few users
 User.create :name => 'Dev Bootcamp Student', :email => 'me@example.com', :password => 'password'
 5.times do
   User.create :name => Faker::Name.name, :email => Faker::Internet.email, :password => 'password'
 end
+
+require 'csv'
+
+class LunchParser
+  attr_reader :file
+
+  def initialize(file)
+    @file = file
+    @lunch_array = CSV.read(@file,{ :headers => true, :header_converters => :symbol })
+    @lunch_array.each do |values|
+    	unless values[:Sponsor_EntityID] == nil
+      Lunch.create!(sponsor_id: values[:Sponsor_EntityID], sponsor_ctds: values[:Sponsor_CTDS], sponsor_name: values[:Sponsor_Name], site_id: values[:Site_EntityID], site_ctds: values[:Site_CTDS], site_name: values[:Site_Name], percentage_eligible: values[:Percent_Eligible_for_Meals], enrollment: values[:Total_Enrolled_as_of_Last_Day_in_October])
+   	 end
+    end
+  end
+end
+
+LunchParser.new("db/2010.csv")
+LunchParser.new("db/2011.csv")
+LunchParser.new("db/2012.csv")
+LunchParser.new("db/2013.csv")
+LunchParser.new("db/2014.csv")
