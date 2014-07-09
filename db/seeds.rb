cabbd3a28f6caef6f5e22ea494f4d4311ef8695f
Joey Rosztoczy <joeyr@stotzeq.com>
2014-07-09 15:00:22 -0500

    removed AR use, parsing with js

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,30 +7,30 @@ User.create :name => 'Dev Bootcamp Student', :email => 'me@example.com', :passwo
   User.create :name => Faker::Name.name, :email => Faker::Internet.email, :password => 'password'
 end
 
-require 'csv'
+# require 'csv'
 
-class LunchParser
-  attr_reader :file
+# class LunchParser
+#   attr_reader :file
 
-  def initialize(file, year)
-    @file = file
-    @lunch_array = CSV.read(@file,{ :headers => true, :header_converters => :symbol })
-    @lunch_array.each do |values|
-    	# puts values.headers
-      unless values[:sponsor_entityid] == nil
-        Lunch.create!(sponsor_id: values[:sponsor_entityid], sponsor_ctds: values[:sponsor_ctds], sponsor_name: values[:sponsor_name], site_id: values[:site_entityid], site_ctds: values[:site_ctds], site_name: values[:site_name], percentage_eligible: values[:percent_eligible_for_meals], enrollment: values[:total_enrolled_as_of_last_day_in_october], school_year: year)
-      end
-    end
-  end
-end
+#   def initialize(file, year)
+#     @file = file
+#     @lunch_array = CSV.read(@file,{ :headers => true, :header_converters => :symbol })
+#     @lunch_array.each do |values|
+#     	# puts values.headers
+#       unless values[:sponsor_entityid] == nil
+#         Lunch.create!(sponsor_id: values[:sponsor_entityid], sponsor_ctds: values[:sponsor_ctds], sponsor_name: values[:sponsor_name], site_id: values[:site_entityid], site_ctds: values[:site_ctds], site_name: values[:site_name], percentage_eligible: values[:percent_eligible_for_meals], enrollment: values[:total_enrolled_as_of_last_day_in_october], school_year: year)
+#       end
+#     end
+#   end
+# end
 
 
-puts "WHat UPPP"
+# puts "WHat UPPP"
 
-# SHOULD REFACTOR TO BE DYNAMIC
+# # SHOULD REFACTOR TO BE DYNAMIC
 
-LunchParser.new("db/2010.csv", 2010)
-LunchParser.new("db/2011.csv", 2011)
-LunchParser.new("db/2012.csv", 2012)
-LunchParser.new("db/2013.csv", 2013)
-LunchParser.new("db/2014.csv", 2014)
+# LunchParser.new("db/2010.csv", 2010)
+# LunchParser.new("db/2011.csv", 2011)
+# LunchParser.new("db/2012.csv", 2012)
+# LunchParser.new("db/2013.csv", 2013)
+# LunchParser.new("db/2014.csv", 2014)
--- public/js/maps.js
+++ public/js/maps.js
@@ -6,4 +6,45 @@ $(document).ready(function(){
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18
 }).addTo(map);
+
+
+	map._initPathRoot() 
+	/* We simply pick up the SVG from the map object */
+	var svg = d3.select("#map").select("svg"),
+	g = svg.append("g");
+
+	var circles = {"objects":[
+		{"circle": {"coordinates": [33.45, -112.066]}} ]}
+
+
+	d3.json("example.json", function(collection) {
+		/* Add a LatLng object to each item in the dataset */
+
+		console.log(collection);
+		collection.objects.forEach(function(d) {
+			d.LatLng = new L.LatLng(d.circle.coordinates[0],
+									d.circle.coordinates[1])
+		})
+		
+		var feature = g.selectAll("circle")
+			.data(collection.objects)
+			.enter().append("circle")
+			.style("stroke", "black")  
+			.style("opacity", .6) 
+			.style("fill", "red")
+			.attr("r", 20);  
+		
+		map.on("viewreset", update);
+		update();
+
+		function update() {
+			feature.attr("transform", 
+			function(d) { 
+				return "translate("+ 
+					map.latLngToLayerPoint(d.LatLng).x +","+ 
+					map.latLngToLayerPoint(d.LatLng).y +")";
+				}
+			)
+		}
+	})	
 });
